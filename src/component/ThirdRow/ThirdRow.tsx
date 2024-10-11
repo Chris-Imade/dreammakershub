4ba75d7e8290931671bc4d0fef5feca1bb6ce4d7
@Chrisdevcode <jephthahimade@gmail.com>
2024-10-12 00:51:03 +0100

    added dropdown menu

--- src/component/ThirdRow/ThirdRow.tsx
+++ src/component/ThirdRow/ThirdRow.tsx
@@ -1,16 +1,32 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { icons } from "../../../public/icons";
 import styles from "./styles.module.scss";
 import Magnetic from "../AnimationCombo/Magnetic/Magnetic";
 
 const ThirdRow = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   const links = ["Home", "About", "Courses", "Contact"];
+
+  const programLinks = [
+    { name: "Data Science", path: "/courses/data-science/" },
+    { name: "Data Analytics", path: "/courses/data-analytics/" },
+    { name: "Machine Learning", path: "/courses/ml/" },
+    { name: "Artificial Intelligence", path: "/courses/ai/" },
+  ];
+
+  // Toggles the dropdown open/close state
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
 
   return (
     <div className={styles.thirdRow}>
       <div className={styles.left}>
-        <button>
+        <button className="relative" onClick={toggleDropdown}>
           <div>
             <Image alt={"book"} src={icons.book} width={28.3} height={28.3} />
             <p>Programs</p>
@@ -21,13 +37,38 @@ const ThirdRow = () => {
             width={28.3}
             height={28.3}
           />
+
+          {/* Dropdown Menu for Programs */}
+          {isDropdownOpen && (
+            <div className={styles.dropdown}>
+              <ul className={styles.dd_list}>
+                {programLinks.map((program, index) => (
+                  <li className="hover:cursor-pointer" key={index}>
+                    <Magnetic>
+                      <a
+                        target="_blank"
+                        referrerPolicy="no-referrer"
+                        href={program.path}
+                      >
+                        {program.name}
+                      </a>
+                    </Magnetic>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </button>
+
         <div>
           <ul>
             {links.map((link, i) => (
               <li key={i}>
                 <Magnetic>
-					<a className={"active"} href={i === 0 ? '/' : `#${link.toLowerCase()}`}>
+                  <a
+                    className={i === 0 ? "active" : ""}
+                    href={i === 0 ? "/" : `#${link.toLowerCase()}`}
+                  >
                     {link}
                   </a>
                 </Magnetic>
@@ -36,6 +77,7 @@ const ThirdRow = () => {
           </ul>
         </div>
       </div>
+
       <Magnetic>
         <button className={styles.right}>
           <p className={styles.text}>Join Now</p>
