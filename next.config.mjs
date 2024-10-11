89da8d019ebde05c6bb07041b48f674a247d9e02
@Chrisdevcode <jephthahimade@gmail.com>
2024-10-11 20:15:12 +0100

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // Export the app as static HTML
+    trailingSlash: true, // Adds a trailing slash to all paths for consistency
+    images: {
+      unoptimized: true, // Disable image optimization for Cloudflare Pages
+    },
+  };
+  
+  export default nextConfig;
+  
